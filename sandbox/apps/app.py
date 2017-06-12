2e6619f7c52f25a35ab158bd9db83685abb228aa
Tarashish Mishra <sunu0000@gmail.com>
2017-06-12 18:09:20 +0530

    Clean up the sandbox app

--- sandbox/apps/app.py
+++ sandbox/apps/app.py
@@ -1,10 +1,4 @@
 from oscar.app import Shop
 
-from apps.checkout.app import application as checkout_app
 
-
-class PayPalShop(Shop):
-    checkout_app = checkout_app
-
-
-application = PayPalShop()
+application = Shop()
--- sandbox/settings.py
+++ sandbox/settings.py
@@ -1,44 +1,39 @@
 import os
 
+from django.utils.translation import ugettext_lazy as _
+
+from oscar.defaults import *
+from oscar import OSCAR_MAIN_TEMPLATE_DIR, get_core_apps
+
+import keys
+
 # Django settings for oscar project.
 PROJECT_DIR = os.path.dirname(__file__)
-location = lambda x: os.path.join(os.path.dirname(os.path.realpath(__file__)), x)
+location = lambda x: os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), x
+)
 
 DEBUG = True
 SQL_DEBUG = True
 
-ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
-)
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 USE_TZ = True
 
-MANAGERS = ADMINS
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': location('db.sqlite'),                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': location('db.sqlite'),
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 ATOMIC_REQUESTS = True
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
 TIME_ZONE = 'Europe/London'
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-gb'
 
 gettext_noop = lambda s: s
@@ -65,27 +60,13 @@ LANGUAGES = (
 
 SITE_ID = 1
 
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
 USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
 USE_L10N = True
 
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = location("public/media")
 
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_ROOT = location("public/media")
 MEDIA_URL = '/media/'
 
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-#ADMIN_MEDIA_PREFIX = '/media/admin/'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = location('public')
@@ -108,8 +89,6 @@ MIDDLEWARE_CLASSES = (
 INTERNAL_IPS = ('127.0.0.1',)
 
 ROOT_URLCONF = 'urls'
-
-from oscar import OSCAR_MAIN_TEMPLATE_DIR
 
 TEMPLATES = [
     {
@@ -139,11 +118,6 @@ TEMPLATES = [
     },
 ]
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -204,7 +178,6 @@ LOGGING = {
     }
 }
 
-
 INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -221,10 +194,8 @@ INSTALLED_APPS = [
     'widget_tweaks',
 ]
 
-from oscar import get_core_apps
 INSTALLED_APPS = INSTALLED_APPS + get_core_apps([
-    'apps.shipping',
-    'apps.checkout'])
+    'apps.shipping'])
 
 AUTHENTICATION_BACKENDS = (
     'oscar.apps.customer.auth_backends.Emailbackend',
@@ -235,13 +206,11 @@ LOGIN_REDIRECT_URL = '/accounts/'
 APPEND_SLASH = True
 
 # Oscar settings
-from oscar.defaults import *
 OSCAR_ALLOW_ANON_CHECKOUT = True
 
 OSCAR_SHOP_TAGLINE = 'Razorpay'
 
 # Add Payflow dashboard stuff to settings
-from django.utils.translation import ugettext_lazy as _
 OSCAR_DASHBOARD_NAVIGATION.append(
     {
         'label': _('Razorpay'),
@@ -262,8 +231,6 @@ HAYSTACK_CONNECTIONS = {
         'ENGINE': 'haystack.backends.simple_backend.SimpleEngine',
     },
 }
-
-import keys
 
 RAZORPAY_API_KEY = keys.RAZORPAY_API_KEY
 RAZORPAY_API_SECRET = keys.RAZORPAY_API_SECRET
